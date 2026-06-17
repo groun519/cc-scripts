42ca94caa1535b7cc2e49aee79c72e03700b0412
groun519 <groun519@gmail.com>
2026-06-17 18:36:22 +0900

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -1,17 +1,17 @@
-local speaker=peripheral.find("speaker")
+local speaker = peripheral.wrap("left")
 
-local decoder=require("cc.audio.dfpwm").make_decoder()
+local decoder = require("cc.audio.dfpwm").make_decoder()
 
-local file=fs.open("music.dfpwm","rb")
+local file = fs.open("music","rb")
 
 while true do
- local chunk=file.read(16*1024)
+ local chunk = file.read(16*1024)
 
  if not chunk then
   break
  end
 
- local buffer=decoder(chunk)
+ local buffer = decoder(chunk)
 
  while not speaker.playAudio(buffer) do
   os.pullEvent("speaker_audio_empty")
